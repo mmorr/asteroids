accdab98028ffe69eaaa4372cb94bb5dd5ac7e87
Mark Morrison <mark@morrison.io>
2024-11-05 15:04:57 -0800

    game loop. gitignore

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
+from constants import *
 
+def main():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-def main:
-    print("Starting asteroids")
-
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            screen.fill("black")
+            pygame.display.flip()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
